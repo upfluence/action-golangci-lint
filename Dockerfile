ac39c9d7d4c733ba8bf3b8e7cdaa2b99cceddc2f
Julien Levesy <jlevesy@gmail.com>
2021-01-25 20:12:30 +0100

    {Dockerfile,entrypoint.sh,golangci.yml}: add a configuration file for golangci-lint

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ ENV REVIEWDOG_VERSION=v0.11.0
 RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b $(go env GOPATH)/bin ${REVIEWDOG_VERSION} 
 
 COPY entrypoint.sh /entrypoint.sh
+COPY golangci.yml /golangci.yml
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,14 @@ cd "${GITHUB_WORKSPACE}/" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 export FILTER_MODE=added
-export FAILED_ON_ERROR=true
+export FAIL_ON_ERROR=true
 export LEVEL=error
 export REPORTER=github-pr-review
-export MODULE_DOWNLOAD_MODE=""
 
-if [ -n "$INPUT_MODULE_DOWNLOAD_MODE" ]; then
-    export MODULE_DOWNLOAD_MODE="--modules-download-mode ${INPUT_MODULE_DOWNLOAD_MODE}"
-fi
-
-golangci-lint run ${MODULE_DOWNLOAD_MODE} --timeout 10m --out-format line-number --enable-all --disable wsl,gochecknoglobals,lll,scopelint,gomnd,funlen,testpackage,goerr113 \
+golangci-lint run -c /golangci.yml
   | reviewdog -f=golangci-lint \
       -name="golangci-linter" \
       -reporter="${REPORTER}" \
       -filter-mode="${FILTER_MODE}" \
-      -fail-on-error="${FAILED_ON_ERROR}" \
+      -fail-on-error="${FAIL_ON_ERROR}" \
       -level="${LEVEL}"
